c35c10944d8d844a6cf372f6c7cf3bb75f38060c
Nick Stenning <nick@whiteink.com>
2023-05-17 16:53:40 +0200

    Add a "blocking" lock acquisition method to Locker
    The scare quotes are because while the interface is blocking, Redis does
    not offer a blocking SETNX, so under the hood this just tries to acquire
    the lock every 50ms.

--- lock/lock.go
+++ lock/lock.go
@@ -17,6 +17,8 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
+const retryInterval = 50 * time.Millisecond
+
 var releaseScript = redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("del", KEYS[1]) else return 0 end`)
 
 var ErrLockNotAcquired = errors.New("locker: did not acquire lock")
@@ -47,6 +49,34 @@ func (l Locker) Prepare(ctx context.Context) error {
 		return err
 	}
 	return nil
+}
+
+// Acquire will attempt to acquire a lock at the specified key in Redis for the
+// given duration. If it fails to acquire the lock because someone else is
+// already holding it, it will retry until the passed context is canceled. If
+// the context is canceled before the lock is acquired it will return the
+// context error. It may also return other errors if it cannot communicate with
+// Redis.
+//
+// Note: it is critical that the caller controls the blocking time by passing in
+// a context that is cancelable or which has a deadline. If the context is never
+// canceled and the lock cannot be acquired, the function will never return.
+func (l Locker) Acquire(ctx context.Context, key string, ttl time.Duration) (Lock, error) {
+	for {
+		lock, err := l.TryAcquire(ctx, key, ttl)
+		if err == nil {
+			return lock, nil
+		}
+		if err != ErrLockNotAcquired {
+			return nil, err
+		}
+
+		select {
+		case <-time.After(retryInterval):
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		}
+	}
 }
 
 // TryAcquire attempts to acquire a lock at the specified key in Redis for the
--- lock/lock_test.go
+++ lock/lock_test.go
@@ -150,6 +150,64 @@ func TestLockReleaseReturnsRedisErrors(t *testing.T) {
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
+func TestLockAcquireIntegration(t *testing.T) {
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		t.Skip("REDIS_URL is not set")
+	}
+
+	ctx := context.Background()
+
+	opts, err := redis.ParseURL(redisURL)
+	require.NoError(t, err)
+
+	client := redis.NewClient(opts)
+	locker := Locker{Client: client}
+
+	require.NoError(t, locker.Prepare(ctx))
+
+	start := make(chan struct{})
+	results := make(chan string, 50)
+	var wg sync.WaitGroup
+
+	// Start 50 goroutines which all attempt to acquire the lock at the same
+	// moment, synchronized by a channel closure.
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+
+		go func(id int) {
+			defer wg.Done()
+			<-start
+
+			// Each goroutine will wait for up to 1s for the lock...
+			timeoutCtx, cancel := context.WithTimeout(ctx, 1*time.Second)
+			defer cancel()
+
+			lock, err := locker.Acquire(timeoutCtx, "monkey", 2*time.Second)
+			if errors.Is(err, context.DeadlineExceeded) {
+				return
+			}
+			require.NoError(t, err)
+
+			// ...and will hold onto it for approximately 100 milliseconds.
+			results <- fmt.Sprintf("lock acquired by goroutine %d", id)
+			time.Sleep(100 * time.Millisecond)
+
+			require.NoError(t, lock.Release(ctx))
+		}(i)
+	}
+
+	// Release the goroutines!
+	close(start)
+	wg.Wait()
+
+	// With each lock held for ~100ms, somewhere between 9 and 11 goroutines
+	// should have got the lock.
+	l := len(results)
+	require.GreaterOrEqual(t, l, 9)
+	require.LessOrEqual(t, l, 11)
+}
+
 func TestLockTryAcquireIntegration(t *testing.T) {
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
