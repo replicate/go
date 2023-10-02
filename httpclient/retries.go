6645efdc8f3cbdbaa75d1aeb961e852413a4347b
Mattt <mattt@replicate.com>
2023-10-02 06:49:19 -0700

    Add `ApplyRetryPolicy` method to `httpclient` package (#62)
    * Add ApplyRetryPolicy method to httpclient package
    
    Co-authored-by: Nick Stenning <nick@whiteink.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Nick Stenning <nick@whiteink.com>
    
    ---------
    
    Co-authored-by: Nick Stenning <nick@whiteink.com>

---
+++ httpclient/retries.go
@@ -0,0 +1,33 @@
+package httpclient
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+// ApplyRetryPolicy wraps an HTTPClient with one that has a retry policy.
+// Currently the retry policy will retry in any of the following scenarios:
+//
+// - connection errors and timeouts
+// - 429 or 503 responses (Retry-After is respected)
+// - other 5XX responses (except 501)
+//
+// All other responses are not retried.
+//
+// With a minimum wait time of 100ms, and a maximum of 4 retries, the
+// inter-retry intervals will be 100ms, 200ms, 400ms, 800ms with the default
+// backoff policy, for a total time of 1500ms (excluding request attempt times).
+func ApplyRetryPolicy(c *http.Client) *http.Client {
+	retryClient := &retryablehttp.Client{
+		HTTPClient:   c,
+		Logger:       nil, // "logging" is assumed to be provided by an OTel transport on the underlying client
+		RetryWaitMin: 100 * time.Millisecond,
+		RetryWaitMax: 2 * time.Second,
+		RetryMax:     4,
+		CheckRetry:   retryablehttp.DefaultRetryPolicy,
+		Backoff:      retryablehttp.DefaultBackoff,
+	}
+	return retryClient.StandardClient()
+}
