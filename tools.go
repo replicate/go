c30872dfce9a9d78912078219c66ed2611111755
Nick Stenning <nick@whiteink.com>
2023-11-16 13:24:30 +0100

    Propagate Replicate-specific trace options in W3C tracestate
    This sets us up to propagate our own Replicate-specific trace options in
    the standard trace state object used by OpenTelemetry. This means that
    it will automatically be propagated by all the standard OTel
    instrumentation libraries.
    
    It adds two new functions to the telemetry package:
    
    1. `TraceOptionsFromContext`, which extracts the trace options from the
       current span context if present.
    
    2. `WithTraceOptions`, which adds trace options to the current context.
    
    There is one additional convenience function, `WithFullTrace`, which
    enables "full trace" mode. This sets the detail level trace option to
    "full" and the sample mode trace option to "always".

--- tools.go
+++ tools.go
@@ -5,5 +5,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/stringer"
 	_ "gotest.tools/gotestsum"
 )
