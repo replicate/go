d1c047d88159fbca576fd0b8468db50d2a4accb1
Nick Stenning <nick@whiteink.com>
2024-06-17 13:16:00 +0200

    Update debug server to be useful
    This makes the debug server actually do something useful by default. See
    the package docstring for more details.

--- debug/server.go
+++ debug/server.go
@@ -1,49 +1,95 @@
 package debug
 
 import (
-	"context"
+	"fmt"
+	"mime"
 	"net/http"
 	"net/http/pprof"
-	"sync"
+	"os"
+	"strings"
 
 	"github.com/replicate/go/logging"
 )
 
-var logger = logging.New("debug")
+const Addr = "localhost:7878"
 
-var defaultServeMux http.ServeMux
+var (
+	logger = logging.New("debug")
+
+	mux      http.ServeMux
+	patterns []string
+)
 
 func init() {
-	defaultServeMux.HandleFunc("/debug/pprof/", pprof.Index)
-	defaultServeMux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	defaultServeMux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	defaultServeMux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	defaultServeMux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/", Index)
+
+	HandleFunc("POST /disable", func(w http.ResponseWriter, _ *http.Request) {
+		Enabled.Store(false)
+		fmt.Fprintln(w, "debug mode disabled")
+	})
+	HandleFunc("/debug/pprof/", pprof.Index)
+	HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	HandleFunc("/debug/pprof/profile", pprof.Profile)
+	HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	HandleFunc("/debug/pprof/trace", pprof.Trace)
+	HandleFunc("/log/level", logging.LevelHandler)
+
+	s := &http.Server{
+		Addr:    Addr,
+		Handler: enabledMiddleware(&mux),
+	}
+
+	logger.Sugar().Infof("debug server listening on %s", Addr)
+	go func() {
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Sugar().Errorw("debug server exited with error", "error", err)
+		}
+	}()
 }
 
-type Server struct {
-	Addr string
-
-	once   sync.Once
-	server *http.Server
+func Handle(pattern string, handler http.Handler) {
+	patterns = append(patterns, pattern)
+	mux.Handle(pattern, handler)
 }
 
-func (s *Server) init() {
-	s.server = &http.Server{
-		Addr:    s.Addr,
-		Handler: &defaultServeMux,
+func HandleFunc(pattern string, handler http.HandlerFunc) {
+	patterns = append(patterns, pattern)
+	mux.HandleFunc(pattern, handler)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	wantHTML := false
+
+	for _, t := range strings.Split(r.Header.Get("Accept"), ",") {
+		mediatype, _, err := mime.ParseMediaType(t)
+		if err == nil && mediatype == "text/html" {
+			wantHTML = true
+			break
+		}
+	}
+
+	// If Accept is text/html, render HTML, otherwise render plain text.
+	if wantHTML {
+		for _, pattern := range patterns {
+			fmt.Fprintf(w, "<a href=\"%s\">%s</a><br>", pattern, pattern)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "Available debug endpoints:\n\n")
+
+	for _, pattern := range patterns {
+		fmt.Fprintf(w, "  %s\n", pattern)
 	}
 }
 
-func (s *Server) ListenAndServe() error {
-	s.once.Do(s.init)
+func enabledMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if Enabled.Load() {
+			next.ServeHTTP(w, r)
+			return
+		}
 
-	logger.Sugar().Infow("starting debug http server", "address", s.Addr)
-	return s.server.ListenAndServe()
-}
-
-func (s *Server) Shutdown(ctx context.Context) error {
-	s.once.Do(s.init)
-
-	return s.server.Shutdown(ctx)
+		http.Error(w, fmt.Sprintf("debug mode disabled (pid: %d)", os.Getpid()), http.StatusForbidden)
+	})
 }
