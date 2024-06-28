d720f132d114793e593e92d730d8ad6f7718b046
Nick Stenning <nick@whiteink.com>
2024-06-28 15:03:32 +0200

    Add OTel metrics support
    This adds a helper `telemetry.Meter` to parallel `telemetry.Tracer`, and
    sets up a default metrics implementation which:
    
    - exports to OTLP every 10s if OTEL_EXPORTER_OTLP_ENDPOINT is configured
    - exposes metrics on :9090/metrics

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -5,7 +5,8 @@ import (
 	"os"
 
 	"go.opentelemetry.io/otel"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/replicate/go/logging"
 )
@@ -14,16 +15,21 @@ var logger = logging.New("telemetry")
 
 func init() {
 	if os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT") == "" {
-		logger.Warn("using no-op tracer provider (OTEL_EXPORTER_OTLP_ENDPOINT is not set)")
+		logger.Warn("metrics/traces will not be exported via OTLP (OTEL_EXPORTER_OTLP_ENDPOINT is not set)")
+		configureMeterProvider(false)
 		return
 	}
 
+	configureMeterProvider(true)
 	configureTracerProvider()
 }
 
 func Shutdown(ctx context.Context) error {
-	if tp, ok := otel.GetTracerProvider().(*sdktrace.TracerProvider); ok {
+	if tp, ok := otel.GetTracerProvider().(*trace.TracerProvider); ok {
 		return tp.Shutdown(ctx)
+	}
+	if mp, ok := otel.GetMeterProvider().(*metric.MeterProvider); ok {
+		return mp.Shutdown(ctx)
 	}
 	return nil
 }
