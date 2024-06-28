d9f84782f9a026d1a6ff27314299b92039ffb674
Nick Stenning <nick@whiteink.com>
2024-06-28 15:03:32 +0200

    Add OTel metrics support
    This adds a helper `telemetry.Meter` to parallel `telemetry.Tracer`, and
    sets up a default metrics implementation which:
    
    - exports to OTLP every 10s if OTEL_EXPORTER_OTLP_ENDPOINT is configured
    - exposes metrics on :9090/metrics

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.7
 	github.com/launchdarkly/go-sdk-common/v3 v3.1.0
 	github.com/launchdarkly/go-server-sdk/v6 v6.2.0
+	github.com/prometheus/client_golang v1.19.1
 	github.com/redis/go-redis/v9 v9.5.2
 	github.com/segmentio/ksuid v1.0.4
 	github.com/speps/go-hashids/v2 v2.0.1
@@ -16,9 +17,13 @@ require (
 	go.opentelemetry.io/contrib/detectors/gcp v1.27.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0
 	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.49.0
+	go.opentelemetry.io/otel/metric v1.27.0
 	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/sdk/metric v1.27.0
 	go.opentelemetry.io/otel/trace v1.27.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.25.0
@@ -27,6 +32,7 @@ require (
 require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/detectors/gcp v1.23.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -49,7 +55,9 @@ require (
 	github.com/onsi/gomega v1.33.1 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.53.0 // indirect
+	github.com/prometheus/procfs v0.15.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
---
+++ telemetry/metric.go
@@ -0,0 +1,92 @@
+package telemetry
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.uber.org/zap"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/replicate/go/version"
+)
+
+// We serve metrics for Prometheus over HTTP. If we appear to be running inside
+// Kubernetes, bind to the wildcard address. Otherwise, bind to localhost.
+//
+// While it has some security benefits, this is mainly to prevent macOS from
+// constantly badgering the user in the development environment.
+var Addr = func() string {
+	if _, ok := os.LookupEnv("KUBERNETES_SERVICE_HOST"); ok {
+		return ":9090"
+	}
+	return "localhost:9090"
+}()
+
+// Meter fetches a meter, applying a standard naming convention for use across
+// services.
+func Meter(service string, component string, opts ...metric.MeterOption) metric.Meter {
+	name := fmt.Sprintf("replicate/%s/%s", service, component)
+	opts = append(opts, metric.WithInstrumentationVersion(version.Version()))
+	return otel.Meter(name, opts...)
+}
+
+func configureMeterProvider(enableOTLP bool) {
+	mp, err := createMeterProvider(context.Background(), enableOTLP)
+	if err != nil {
+		logger.Warn("failed to create meter provider", zap.Error(err))
+		return
+	}
+
+	go serveMetrics()
+
+	otel.SetMeterProvider(mp)
+}
+
+func createMeterProvider(ctx context.Context, enableOTLP bool) (metric.MeterProvider, error) {
+	opts := []sdkmetric.Option{
+		sdkmetric.WithResource(DefaultResource()),
+	}
+
+	// Always export metrics to Prometheus.
+	prom, err := prometheus.New()
+	if err != nil {
+		return nil, err
+	}
+	opts = append(opts, sdkmetric.WithReader(prom))
+
+	// If enabled, export metrics to OTLP as well, every 10s.
+	if enableOTLP {
+		exp, err := otlpmetrichttp.New(ctx)
+		if err != nil {
+			return nil, err
+		}
+		reader := sdkmetric.NewPeriodicReader(exp, sdkmetric.WithInterval(10*time.Second))
+		opts = append(opts, sdkmetric.WithReader(reader))
+	}
+
+	mp := sdkmetric.NewMeterProvider(opts...)
+	return mp, nil
+}
+
+func serveMetrics() {
+	mux := http.ServeMux{}
+	mux.Handle("/metrics", promhttp.Handler())
+
+	s := &http.Server{
+		Addr:    Addr,
+		Handler: &mux,
+	}
+
+	logger.Sugar().Infof("metrics server listening on %s", Addr)
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		logger.Sugar().Errorw("metrics server exited with error", "error", err)
+	}
+}
