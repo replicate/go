cce396a641163e1aeeb21139b0485f323db54050
Nick Stenning <nick@whiteink.com>
2024-07-15 11:19:00 +0200

    Write some proper package documentation for queue

--- queue/client.go
+++ queue/client.go
@@ -2,7 +2,6 @@ package queue
 
 import (
 	"context"
-	_ "embed"
 	"fmt"
 	"strings"
 	"time"
@@ -13,21 +12,14 @@ import (
 )
 
 var (
-	//go:embed len.lua
-	lenCmd    string
-	lenScript = redis.NewScript(lenCmd)
-
-	//go:embed read.lua
-	readCmd    string
-	readScript = redis.NewScript(readCmd)
-
-	//go:embed write.lua
-	writeCmd    string
-	writeScript = redis.NewScript(writeCmd)
-
 	ErrInvalidReadArgs  = fmt.Errorf("queue: invalid read arguments")
 	ErrInvalidWriteArgs = fmt.Errorf("queue: invalid write arguments")
 )
+
+type Client struct {
+	rdb redis.Cmdable
+	ttl time.Duration // ttl for all keys in queue
+}
 
 func NewClient(rdb redis.Cmdable, ttl time.Duration) *Client {
 	return &Client{
@@ -39,41 +31,39 @@ func NewClient(rdb redis.Cmdable, ttl time.Duration) *Client {
 // Prepare stores the write and read scripts in the Redis script cache so that
 // they can be more efficiently called with EVALSHA.
 func (c *Client) Prepare(ctx context.Context) error {
-	if err := lenScript.Load(ctx, c.rdb).Err(); err != nil {
-		return err
-	}
-	if err := readScript.Load(ctx, c.rdb).Err(); err != nil {
-		return err
-	}
-	if err := writeScript.Load(ctx, c.rdb).Err(); err != nil {
-		return err
-	}
-	return nil
+	return prepare(ctx, c.rdb)
 }
 
+// Len calculates the aggregate length (XLEN) of the queue. It adds up the
+// lengths of all the streams in the queue.
 func (c *Client) Len(ctx context.Context, name string) (int64, error) {
 	return lenScript.RunRO(ctx, c.rdb, []string{name}).Int64()
 }
 
-func (c *Client) Read(ctx context.Context, a *ReadArgs) (*Message, error) {
-	if a == nil {
+// Read a single message from the queue. If the Block field of args is
+// non-zero, the call may block for up to that duration waiting for a new
+// message.
+//
+// If no message is available both return values will be nil.
+func (c *Client) Read(ctx context.Context, args *ReadArgs) (*Message, error) {
+	if args == nil {
 		return nil, fmt.Errorf("%w: args cannot be nil", ErrInvalidReadArgs)
 	}
-	if a.Name == "" {
+	if args.Name == "" {
 		return nil, fmt.Errorf("%w: name cannot be empty", ErrInvalidReadArgs)
 	}
-	if a.Group == "" {
+	if args.Group == "" {
 		return nil, fmt.Errorf("%w: group cannot be empty", ErrInvalidReadArgs)
 	}
-	if a.Consumer == "" {
+	if args.Consumer == "" {
 		return nil, fmt.Errorf("%w: consumer cannot be empty", ErrInvalidReadArgs)
 	}
 
-	return c.read(ctx, a)
+	return c.read(ctx, args)
 }
 
-func (c *Client) read(ctx context.Context, a *ReadArgs) (*Message, error) {
-	result, err := c.readOnce(ctx, a)
+func (c *Client) read(ctx context.Context, args *ReadArgs) (*Message, error) {
+	result, err := c.readOnce(ctx, args)
 	if err != nil {
 		return nil, err
 	}
@@ -81,13 +71,14 @@ func (c *Client) read(ctx context.Context, a *ReadArgs) (*Message, error) {
 		return result, nil
 	}
 
-	// If we get here, we got no message, so consider waiting on the notifications
-	// channel for up to the block duration.
-	if a.Block == 0 {
+	// If we're here, the queue was Empty. We only have more work to do if
+	// args.Block is non-zero.
+	if args.Block == 0 {
 		return nil, nil
 	}
 
-	ok, err := c.wait(ctx, a)
+	// Wait for a message to be signaled on the notifications stream.
+	ok, err := c.wait(ctx, args)
 	if err != nil {
 		return nil, err
 	}
@@ -95,13 +86,13 @@ func (c *Client) read(ctx context.Context, a *ReadArgs) (*Message, error) {
 		return nil, nil
 	}
 
-	return c.readOnce(ctx, a)
+	return c.readOnce(ctx, args)
 }
 
-func (c *Client) readOnce(ctx context.Context, a *ReadArgs) (*Message, error) {
-	keys := []string{a.Name}
-	args := []any{int(c.ttl.Seconds()), a.Group, a.Consumer}
-	result, err := readScript.Run(ctx, c.rdb, keys, args...).Result()
+func (c *Client) readOnce(ctx context.Context, args *ReadArgs) (*Message, error) {
+	cmdKeys := []string{args.Name}
+	cmdArgs := []any{int(c.ttl.Seconds()), args.Group, args.Consumer}
+	result, err := readScript.Run(ctx, c.rdb, cmdKeys, cmdArgs...).Result()
 	switch {
 	case err == redis.Nil:
 		return nil, nil
@@ -112,30 +103,30 @@ func (c *Client) readOnce(ctx context.Context, a *ReadArgs) (*Message, error) {
 	return parse(result)
 }
 
-func (c *Client) wait(ctx context.Context, a *ReadArgs) (bool, error) {
-	ok, err := c.waitOnce(ctx, a)
+func (c *Client) wait(ctx context.Context, args *ReadArgs) (bool, error) {
+	ok, err := c.waitOnce(ctx, args)
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "NOGROUP") {
-			stream := a.Name + ":notifications"
-			if err := c.rdb.XGroupCreateMkStream(ctx, stream, a.Group, "0").Err(); err != nil {
+			stream := args.Name + ":notifications"
+			if err := c.rdb.XGroupCreateMkStream(ctx, stream, args.Group, "0").Err(); err != nil {
 				return false, err
 			}
 			// If we create the stream, we're responsible for expiring it.
 			if err := c.rdb.Expire(ctx, stream, c.ttl).Err(); err != nil {
 				return false, err
 			}
-			return c.waitOnce(ctx, a)
+			return c.waitOnce(ctx, args)
 		}
 	}
 	return ok, err
 }
 
-func (c *Client) waitOnce(ctx context.Context, a *ReadArgs) (bool, error) {
+func (c *Client) waitOnce(ctx context.Context, args *ReadArgs) (bool, error) {
 	err := c.rdb.XReadGroup(ctx, &redis.XReadGroupArgs{
-		Group:    a.Group,
-		Consumer: a.Consumer,
-		Streams:  []string{a.Name + ":notifications", ">"},
-		Block:    a.Block,
+		Group:    args.Group,
+		Consumer: args.Consumer,
+		Streams:  []string{args.Name + ":notifications", ">"},
+		Block:    args.Block,
 		Count:    1,
 		NoAck:    true, // immediately ack so no further handling is required
 	}).Err()
@@ -148,56 +139,58 @@ func (c *Client) waitOnce(ctx context.Context, a *ReadArgs) (bool, error) {
 	return true, nil
 }
 
-func (c *Client) Write(ctx context.Context, a *WriteArgs) (string, error) {
-	if a == nil {
+// Write a message to the queue. The message will be written to the shortest
+// queue in the tenant's shard, which is determined by the ShardKey in args.
+func (c *Client) Write(ctx context.Context, args *WriteArgs) (string, error) {
+	if args == nil {
 		return "", fmt.Errorf("%w: args cannot be nil", ErrInvalidWriteArgs)
 	}
-	if a.Name == "" {
+	if args.Name == "" {
 		return "", fmt.Errorf("%w: name cannot be empty", ErrInvalidWriteArgs)
 	}
-	if a.Streams == 0 {
-		a.Streams = 1
+	if args.Streams == 0 {
+		args.Streams = 1
 	}
-	if a.StreamsPerShard == 0 {
-		a.StreamsPerShard = 1
+	if args.StreamsPerShard == 0 {
+		args.StreamsPerShard = 1
 	}
-	if a.Streams < 0 {
+	if args.Streams < 0 {
 		return "", fmt.Errorf("%w: streams must be > 0", ErrInvalidWriteArgs)
 	}
-	if a.StreamsPerShard < 0 {
+	if args.StreamsPerShard < 0 {
 		return "", fmt.Errorf("%w: streams per shard must be > 0", ErrInvalidWriteArgs)
 	}
-	if a.StreamsPerShard > a.Streams {
+	if args.StreamsPerShard > args.Streams {
 		return "", fmt.Errorf("%w: streams per shard must be <= streams", ErrInvalidWriteArgs)
 	}
-	if len(a.ShardKey) == 0 {
+	if len(args.ShardKey) == 0 {
 		return "", fmt.Errorf("%w: shard key cannot be empty", ErrInvalidWriteArgs)
 	}
-	if len(a.Values) == 0 {
+	if len(args.Values) == 0 {
 		return "", fmt.Errorf("%w: values cannot be empty", ErrInvalidWriteArgs)
 	}
 
-	return c.write(ctx, a)
+	return c.write(ctx, args)
 }
 
-func (c *Client) write(ctx context.Context, a *WriteArgs) (string, error) {
-	shard := shuffleshard.Get(a.Streams, a.StreamsPerShard, a.ShardKey)
+func (c *Client) write(ctx context.Context, args *WriteArgs) (string, error) {
+	shard := shuffleshard.Get(args.Streams, args.StreamsPerShard, args.ShardKey)
 
-	// Args is of length 3 (for seconds, streams, n) + len(shard) + 2*len(values)
-	keys := []string{a.Name}
-	args := make([]any, 0, 3+len(shard)+2*len(a.Values))
+	cmdKeys := []string{args.Name}
+	// Capacity: 3 (for seconds, streams, n) + len(shard) + 2*len(values)
+	cmdArgs := make([]any, 0, 3+len(shard)+2*len(args.Values))
 
-	args = append(args, int(c.ttl.Seconds()))
-	args = append(args, a.Streams)
-	args = append(args, len(shard))
+	cmdArgs = append(cmdArgs, int(c.ttl.Seconds()))
+	cmdArgs = append(cmdArgs, args.Streams)
+	cmdArgs = append(cmdArgs, len(shard))
 	for _, s := range shard {
-		args = append(args, s)
+		cmdArgs = append(cmdArgs, s)
 	}
-	for k, v := range a.Values {
-		args = append(args, k, v)
+	for k, v := range args.Values {
+		cmdArgs = append(cmdArgs, k, v)
 	}
 
-	return writeScript.Run(ctx, c.rdb, keys, args...).Text()
+	return writeScript.Run(ctx, c.rdb, cmdKeys, cmdArgs...).Text()
 }
 
 func parse(v any) (*Message, error) {
---
+++ queue/queue.go
@@ -0,0 +1,67 @@
+// Package queue provides a queue implementation based on Redis streams which
+// uses [shuffle-sharding] to provide a degree of isolation between queue
+// tenants.
+//
+// Shuffle sharding is a technique that divides a shared resource into N virtual
+// resources, and randomly but consistently assigned some M (< N) of those
+// virtual resources to each tenant as its "shard." Only if two tenants share
+// the exact same shard are their fates fully entangled. For reasonably modest
+// values of M and N, the probability of two tenants sharing a shard can be made
+// exceedingly small: 1 / (N choose M).
+//
+// In our implementation, we divide a single logical queue into N "virtual
+// queues," each of which is a separate Redis stream. Each tenant is then
+// assigned M of these virtual queues as their "shard" based on a tenant key.
+// When enqueueing messages, we check the lengths of each stream in the tenant's
+// shard and enqueue to the shortest one. When dequeuing messages, consumers
+// cycle through all the streams in turn.
+//
+// In practice, this means that tenants making heavy use of the queue are
+// straightforwardly avoided by other tenants, so long as those tenants' shards
+// do not overlap fully with the heavy user's shard.
+//
+// One wrinkle is that it is not possible using the Redis stream abstraction to
+// block and wait for a single message across multiple streams. XREADGROUP can
+// do this, but if more than one stream has messages waiting at the moment the
+// call is made, it will claim and return one message per stream, which is not
+// suitable for our use case. To work around this, we use a separate
+// "notifications" stream to signal to consumers that work has been added to the
+// queue. Triggered by this stream, they can then read from the streams that
+// make up the queue.
+//
+// [shuffle-sharding]: https://aws.amazon.com/builders-library/workload-isolation-using-shuffle-sharding/
+package queue
+
+import (
+	"context"
+	_ "embed" // to provide go:embed support
+
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	//go:embed len.lua
+	lenCmd    string
+	lenScript = redis.NewScript(lenCmd)
+
+	//go:embed read.lua
+	readCmd    string
+	readScript = redis.NewScript(readCmd)
+
+	//go:embed write.lua
+	writeCmd    string
+	writeScript = redis.NewScript(writeCmd)
+)
+
+func prepare(ctx context.Context, rdb redis.Cmdable) error {
+	if err := lenScript.Load(ctx, rdb).Err(); err != nil {
+		return err
+	}
+	if err := readScript.Load(ctx, rdb).Err(); err != nil {
+		return err
+	}
+	if err := writeScript.Load(ctx, rdb).Err(); err != nil {
+		return err
+	}
+	return nil
+}
