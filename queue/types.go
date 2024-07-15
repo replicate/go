b0138d4c2c21a23717b53653df0923fa826d3391
Nick Stenning <nick@whiteink.com>
2024-07-15 11:19:00 +0200

    Write some proper package documentation for queue

--- queue/types.go
+++ queue/types.go
@@ -2,36 +2,26 @@ package queue
 
 import (
 	"time"
-
-	"github.com/redis/go-redis/v9"
 )
 
-type Client struct {
-	rdb redis.Cmdable
-
-	// Queue TTL
-	ttl time.Duration
-}
-
 type WriteArgs struct {
-	Name   string
-	Values map[string]any
+	Name   string         // queue name
+	Values map[string]any // message values
 
-	// Shuffle sharding configuration
-	Streams         int
-	StreamsPerShard int
-	ShardKey        []byte
+	Streams         int    // total number of streams
+	StreamsPerShard int    // number of streams in each shard
+	ShardKey        []byte // tenant key to determine shard
 }
 
 type ReadArgs struct {
-	Name     string
-	Group    string
-	Consumer string
+	Name     string        // queue name
+	Group    string        // consumer group name
+	Consumer string        // consumer ID
 	Block    time.Duration // total blocking time
 }
 
 type Message struct {
-	Stream string
+	Stream string // stream from which this message was read
 	ID     string
 	Values map[string]any
 }
